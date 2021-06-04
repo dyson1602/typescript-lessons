75f6aee95d8b8189406c245d4eab5261df8e3d90
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-04 12:57:36 -0400

    new: arrays

---
+++ features/arrays.ts
@@ -0,0 +1,24 @@
+const carMakers = ['ford', 'chevy', 'toyota'];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [['f150'], ['camaro'], ['corolla']];
+
+//Help with type inference
+
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+//prvent incompatible values
+
+carMakers.push(100);
+
+//help with 'map'
+
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+//Flexible types
+const importantDates: (Date | string)[] = [new Date(), '2030-10-10']
+importantDates.push('2030-10-11')
+importantDates.push(new Date())
